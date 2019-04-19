96a5be973265093c911595ebd4cea44e0ad3417d
Agata Stawarz <agata.stawarz94@gmail.com>
2019-04-19 12:00:03 +0200

    Add showing best results using local storage

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="container" id="app">
     <h1 class="quiz-header">Quiz</h1>
+
     <!-- progress bar -->
     <div id="progress-bar">
       <div v-bind:style="{width: progress + '%'}"></div>
     </div>
+
     <!-- show questions when they are loaded from api and the user not passed all questions -->
     <div v-if="questions && currentQuestionIndex < questions.length">
       <h3> {{ questions[currentQuestionIndex].question }}</h3>
@@ -15,10 +17,12 @@
           {{ answer }}
         </button>
     </div>
+
     <!-- show final page when questions have been loaded from api and user passed all questions -->
     <div v-if="questions && currentQuestionIndex === questions.length">
       <img class="done-img" alt="Happy girl putting her hands up" src="../assets/done.png">
       <h3>Your points: {{ points }} </h3>
+      <h3>Your best result: {{ bestPoints }}</h3>
       <button v-on:click="reset">try again</button>
     </div>
   </div>
@@ -34,32 +38,46 @@ export default {
       currentQuestionIndex: 0,
       points: 0,
       progress: 0,
+      bestPoints: 0,
     };
   },
   methods: {
     submitAnswer(answer) {
+      // if submitted answer is the same as correct answer of the given question
       if (answer === this.questions[this.currentQuestionIndex].correct_answer) {
         this.points += 1;
         console.log(`correct ${this.points}`);
+        if (this.points > this.bestPoints) {
+          this.bestPoints = this.points;
+        }
       }
+      // show next question after submitting answer (no matter correct or incorrect)
       this.showNextQuestion();
     },
+
     showNextQuestion() {
       if (this.currentQuestionIndex < this.questions.length) {
         this.currentQuestionIndex += 1;
         this.hitProgress();
       }
     },
+
+    // reset quiz when try again
     reset() {
       this.$router.go();
     },
+
+    // progress of answering question presented on the bar
     hitProgress() {
       this.progress += 10;
     },
+
+    // method for choosing answers randomly
     getRandomInt(min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     },
   },
+
   // method which starts when component is created on the page
   mounted() {
     fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple')
@@ -76,6 +94,21 @@ export default {
           return newQuestion;
         });
       });
+
+    // load favourites from localStorage -
+    // if there are bestPoints in LocalStorage, it parses it and sets the app’s bestPoints
+    if (localStorage.getItem('bestPoints')) {
+      this.bestPoints = localStorage.getItem('bestPoints');
+    }
+  },
+
+  // watch bestPoints - if they change, change the number of items in local storage
+  watch: {
+    bestPoints: {
+      handler() {
+        localStorage.setItem('bestPoints', this.bestPoints);
+      },
+    },
   },
 };
 </script>
